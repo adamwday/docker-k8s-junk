ab6c6f954a7380b80ae98e8446de6ea8a9495007
adamwday <adam.day@outlook.com>
2018-03-02 21:01:48 -0700

    Adding docker files for elk stack

---
+++ elk-stack/dockerimg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#REPO_NAME=$elasticsearch
+#
+#if [ $# -ne elasticsearch]; then
+#   echo $0: usage: $0 REPO_NAME
+#   exit 1
+#fi
+
+#Runs the docker login command
+aws ecr get-login --no-include-email --region us-east-1
+
+#Builds the docker image
+docker built --tag  elasticsearch .
+
+#Tags the docker image before we push it to the ECR.
+docker tag elasticsearch:latest 765717427546.dkr.ecr.us-east-1.amazonaws.com/elasticsearch:latest
+
+#Pushes the docker image to AWS ECR
+docker push 765717427546.dkr.ecr.us-east-1.amazonaws.com/elasticsearch:latest
+  
